e48ca6631149f81f29fbdff7ffffe4d32b8db2fc
Camille Haus <camille_haus@hotmail.com>
2024-12-24 15:59:57 -0300

    Solution added

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { NewTodoField } from './NewTodoField';
+import React from 'react';
+
+export const Header: React.FC = () => {
+  return (
+    <header className="todoapp__header">
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+      />
+
+      <NewTodoField />
+    </header>
+  );
+};
